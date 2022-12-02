e4a3f41e7a42ca904a784565c156a997dd4abeae
Moa2405 <62264443+Moa2405@users.noreply.github.com>
2022-12-02 19:12:03 +0100

    dev13

--- src/components/post/tab.jsx
+++ src/components/post/tab.jsx
@@ -1,80 +1,25 @@
-import { useRef, useState } from "react";
-import { Box, InputAdornment, TextField } from "@mui/material";
-import url from "../../../common/url";
-import { LoadingButton } from "@mui/lab";
-import { useSnackBar } from "../../../context/snackBarContext";
-import useAxios from "../../../hooks/useAxios";
-import { useMutation } from 'react-query';
-
-const CommentOnPost = ({ postId }) => {
-
-  const { activateSnackBar } = useSnackBar();
-  const commentRef = useRef();
-  const [disabled, setDisabled] = useState(true);
-  const axios = useAxios();
-
-  const postComment = async () => {
-    const options = {
-      body: commentRef.current.value
-    }
-    const { data } = await axios.post(url.posts.comment(postId, options));
-    return data;
-  }
-
-  const { isLoading, isError, error, mutate } = useMutation(postComment, {
-    onSuccess: () => {
-      commentRef.current.value = "";
-      setDisabled(true);
-      activateSnackBar("Comment added successfully", "success");
-    },
-    onError: () => {
-      activateSnackBar("Something went wrong", "error");
-    }
-  });
-
-  const handleOnChange = (e) => {
-    e.target.value.length > 0 ? setDisabled(false) : setDisabled(true);
-  }
-
-  const handleComment = () => {
-    mutate();
-  }
-
-
-  return (
-    <Box sx={{ width: "100%" }}>
-      <Box>
-        <TextField
-          id="input-with-icon-textfield"
-          label="Comment..."
-          multiline
-          fullWidth
-          size="small"
-          inputRef={commentRef}
-          defaultValue=""
-          onChange={(e) => { handleOnChange(e) }}
-          InputProps={{
-            disableUnderline: true,
-            endAdornment: (
-              <InputAdornment position="end">
-                <LoadingButton
-                  loading={isLoading}
-                  disabled={disabled}
-                  onClick={handleComment}
-                  sx={{ marginBottom: "16px" }}
-                  variant="text"
-                >
-                  Post
-                </LoadingButton>
-              </InputAdornment>
-            )
-          }}
-          variant="filled"
-          helperText={error}
-        />
-      </Box>
-    </Box>
-  );
-}
-
-export default CommentOnPost;
+// {
+//   "short_name": "React App",
+//   "name": "Create React App Sample",
+//   "icons": [
+//     {
+//       "src": "favicon.ico",
+//       "sizes": "64x64 32x32 24x24 16x16",
+//       "type": "image/x-icon"
+//     },
+//     {
+//       "src": "logo192.png",
+//       "type": "image/png",
+//       "sizes": "192x192"
+//     },
+//     {
+//       "src": "logo512.png",
+//       "type": "image/png",
+//       "sizes": "512x512"
+//     }
+//   ],
+//   "start_url": ".",
+//   "display": "standalone",
+//   "theme_color": "#000000",
+//   "background_color": "#ffffff"
+// }
